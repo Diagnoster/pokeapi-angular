3132699cd2ec92e636b6290958b3acd1c8f132a6
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-01-30 12:20:36 -0300

    Update poke-helper.service.ts

--- src/app/services/poke-helper.service.ts
+++ src/app/services/poke-helper.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class PokeHelperService {
 
   constructor() { }
+
+  upperFirstLetter(word: string): string {
+    if (this.checkHyphen(word)) {
+      word = word.replace(/-/g, ' ');
+    }
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  
+  checkHyphen(palavra: string): boolean {
+    return palavra.includes('-');
+  }
+  
 }
