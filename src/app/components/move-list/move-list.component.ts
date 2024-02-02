dc3a8b26aacbd1db2184a37d84e242e6fec41eac
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-02-02 07:55:50 -0300

    Move list with filter

--- src/app/components/move-list/move-list.component.ts
+++ src/app/components/move-list/move-list.component.ts
@@ -7,13 +7,17 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatTableModule } from '@angular/material/table';
 import { MoveDetails } from '../../models/move-details';
 import { PokeHelperService } from '../../services/poke-helper.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-move-list',
   standalone: true,
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './move-list.component.html',
   styleUrl: './move-list.component.css'
@@ -56,6 +60,11 @@ export class MoveListComponent implements OnInit {
 
   upperFirstLetter(word: string): string {
     return this.pokeHelperService.upperFirstLetter(word);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
