f5abd7ed8aee40b2e38f818ca70a7be21fa96e34
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-02-05 14:22:35 -0300

    move list working

--- src/app/components/move-list/move-list.component.ts
+++ src/app/components/move-list/move-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { PokeServiceService } from '../../services/poke-service.service';
 import { Move } from '../../models/move';
 import { MatTableDataSource } from '@angular/material/table';
@@ -7,6 +7,7 @@ import { MoveDetails } from '../../models/move-details';
 import { PokeHelperService } from '../../services/poke-helper.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-move-list',
@@ -14,17 +15,19 @@ import { MatInputModule} from '@angular/material/input';
   imports: [
     MatTableModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatPaginatorModule
 ],
   templateUrl: './move-list.component.html',
   styleUrl: './move-list.component.css'
 })
-export class MoveListComponent implements OnInit {
+export class MoveListComponent implements AfterViewInit {
 
   moves: Move[];
   moveDetailsList: MoveDetails [];
   displayedColumns = ['id', 'name', 'type', 'power', 'accuracy', 'pp'];
   dataSource = new MatTableDataSource<MoveDetails>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private pokeService: PokeServiceService, private pokeHelperService: PokeHelperService) {
     this.moves = [];
@@ -32,9 +35,10 @@ export class MoveListComponent implements OnInit {
     this.dataSource = new MatTableDataSource();
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
     this.getMoves();
-    
+    this.dataSource = new MatTableDataSource(this.moveDetailsList);
+
   }
 
   getMoves() {
@@ -43,10 +47,11 @@ export class MoveListComponent implements OnInit {
       this.moves.forEach( move => {
         this.pokeService.getPokeMoves(move.url).subscribe((data:any) => {
           this.moveDetailsList.push(data);
+          this.dataSource = new MatTableDataSource(this.moveDetailsList);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.connect();
         });
       })
-      this.dataSource.data = this.moveDetailsList;
-      this.dataSource.connect();
     })
   }
 
@@ -56,11 +61,6 @@ export class MoveListComponent implements OnInit {
   
   upperFirstLetter(word: string): string {
     return this.pokeHelperService.upperFirstLetter(word);
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withFetch()),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations()
   ],
 });
