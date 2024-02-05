f7d1f51cde2ce48652bf190664e79ad096d78b1f
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-02-05 07:46:41 -0300

    background color move poke details adjustment

--- src/app/models/pokemon-move-color.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PokemonMoveColor } from './pokemon-move-color';
-
-describe('PokemonMoveColor', () => {
-  it('should create an instance', () => {
-    expect(new PokemonMoveColor()).toBeTruthy();
-  });
-});
