f3367a362be386290aee2b37ed1e6f418643725e
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-02-07 14:07:46 -0300

    router name component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component, HostListener } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -18,4 +19,21 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  componentName: string = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          const routeComponent = this.getRouteComponent(event.url);
+          this.componentName = routeComponent ? routeComponent.charAt(0).toUpperCase() + routeComponent.slice(1) : '';
+        }
+      });
+  }
+
+  private getRouteComponent(url: string): string | null {
+    const route = url.split('/').pop();
+    return route ? route : null;
+  }
 }
