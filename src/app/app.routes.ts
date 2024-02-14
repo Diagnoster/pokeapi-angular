7b903c790cc5d0e4f38485101826d2138da4267d
James Rovel Barbosa <diagjames@gmail.com>
2024-02-14 11:08:24 -0300

    route home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MoveListComponent } from './components/move-list/move-list.component';
 import { PokeListComponent } from './components/poke-list/poke-list.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 export const routes: Routes = [
-    { path: '', component: MoveListComponent},
+    { path: '', component: HomeComponentComponent},
+    { path: 'home', component: HomeComponentComponent},
     { path: 'moves', component: MoveListComponent},
     { path: 'pokedex', component: PokeListComponent},
     { path: 'items', component: ItemListComponent}
