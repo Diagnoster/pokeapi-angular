d42acee16ea9dcb78a6fbcc2ba73fdc5db469278
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-02-22 09:41:33 -0300

    add header select span after click

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -13,13 +14,15 @@ import { filter } from 'rxjs';
     MatToolbarModule, 
     MatButtonModule, 
     MatIconModule,
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   componentName: string = '';
+  selectedSpan: string | null = null;
 
   constructor(private router: Router) {
     this.router.events
@@ -30,6 +33,12 @@ export class HeaderComponent {
           this.componentName = routeComponent ? routeComponent.charAt(0).toUpperCase() + routeComponent.slice(1) : '';
         }
       });
+  }
+
+  selectSpan(spanId: string) {
+    if (this.selectedSpan !== spanId) {
+      this.selectedSpan = spanId;
+    }
   }
 
   private getRouteComponent(url: string): string | null {
