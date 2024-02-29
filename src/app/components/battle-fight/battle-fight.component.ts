7a773b817a6b8d1b74f9d9386124869ab574760e
James Barbosa <james.barbosa@mps.com.br>
2024-02-29 15:27:06 +0000

    battle component

---
+++ src/app/components/battle-fight/battle-fight.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-battle-fight',
+  standalone: true,
+  imports: [],
+  templateUrl: './battle-fight.component.html',
+  styleUrl: './battle-fight.component.css'
+})
+export class BattleFightComponent {
+
+}
