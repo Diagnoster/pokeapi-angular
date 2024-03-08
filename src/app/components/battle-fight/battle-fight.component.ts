2ff8b72c76cbd97937c366c9577cab20f21ed510
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-03-08 13:52:20 -0300

    battle working

--- src/app/components/battle-fight/battle-fight.component.ts
+++ src/app/components/battle-fight/battle-fight.component.ts
@@ -24,6 +24,8 @@ export class BattleFightComponent implements OnInit {
   enemySelectedMoves: MoveDetails[] = [];
   playerStats: BattleStats = { hp: 0, attack: 0, defense: 0, special_attack: 0, special_defense: 0, speed: 0 };
   enemyStats: BattleStats = { hp: 0, attack: 0, defense: 0, special_attack: 0, special_defense: 0, speed: 0 };
+  selectedMove: MoveDetails | null = null;
+  battleText: string = "Choose a move to use";
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private pokeHelper: PokeHelperService) {
     this.playerPokemon = data.playerPokemon;
@@ -34,35 +36,35 @@ export class BattleFightComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.playerSelectedMoves);
-    this.calcStats(this.playerPokemon, this.playerStats);
-    this.calcStats(this.enemyPokemon, this.enemyStats);
-    this.startBattle();
+    console.log(this.enemySelectedMoves);
+    this.calcStats(this.playerPokemon);
+    this.calcStats2(this.enemyPokemon);
   }
 
   upperFirstLetter(word: string, gen?: boolean): string {
     return this.pokeHelper.upperFirstLetter(word);
   }
 
-  calcStats(pokemon: Pokemon, stats: BattleStats) {
+  calcStats(pokemon: Pokemon) {
     pokemon.stats.forEach((stat, index) => {
       switch (index) {
         case 0:
-          stats.hp = stat.base_stat;
+          this.playerStats.hp = stat.base_stat;
           break;
         case 1:
-          stats.attack = stat.base_stat;
+          this.playerStats.attack = stat.base_stat;
           break;
         case 2:
-          stats.defense = stat.base_stat;
+          this.playerStats.defense = stat.base_stat;
           break;
         case 3:
-          stats.special_attack = stat.base_stat;
+          this.playerStats.special_attack = stat.base_stat;
           break;
         case 4:
-          stats.special_defense = stat.base_stat;
+          this.playerStats.special_defense = stat.base_stat;
           break;
         case 5:
-          stats.speed = stat.base_stat;
+          this.playerStats.speed = stat.base_stat;
           break;
         default:
           break;
@@ -70,15 +72,62 @@ export class BattleFightComponent implements OnInit {
     });
   }
 
+  calcStats2(pokemon: Pokemon) {
+    pokemon.stats.forEach((stat, index) => {
+      switch (index) {
+        case 0:
+          this.enemyStats.hp = stat.base_stat;
+          break;
+        case 1:
+          this.enemyStats.attack = stat.base_stat;
+          break;
+        case 2:
+          this.enemyStats.defense = stat.base_stat;
+          break;
+        case 3:
+          this.enemyStats.special_attack = stat.base_stat;
+          break;
+        case 4:
+          this.enemyStats.special_defense = stat.base_stat;
+          break;
+        case 5:
+          this.enemyStats.speed = stat.base_stat;
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  selectMove(move: MoveDetails) {
+    this.selectedMove = move;
+    this.startBattle();
+  }
+
   startBattle() {
-    while(this.playerStats.hp > 0 && this.enemyStats.hp > 0) {
-      
+    if (!this.selectedMove) {
+      console.error('No move selected!');
+      return;
     }
-    if(this.playerStats.hp === 0) {
-      console.log('player lost');
+    if (this.playerStats.hp > 0 && this.enemyStats.hp > 0) {
+      this.attack(this.playerStats, this.enemyStats, this.selectedMove);
+      if (this.enemyStats.hp > 0) {
+        this.attack(this.enemyStats, this.playerStats, this.enemySelectedMoves[0]);
+      }
+    }
+    if (this.playerStats.hp <= 0) {
+      this.battleText = "Player lost";
     } else {
-      console.log('enemy lost');
+      this.battleText = "Enemy lost";
     }
+  }
+
+  attack(attacker: BattleStats, defender: BattleStats, move: MoveDetails) {
+    if (defender.hp > 0) {
+      defender.hp -= move.power;
+      this.battleText = `attacks with ${move.name} causing ${move.power} damage.`;
+    }
+    this.selectedMove = null;
   }
 
 }
