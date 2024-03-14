ec294ed784f5e78de3c1bafea24595382020743e
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-03-14 12:16:05 -0300

    ability service

--- src/app/components/ability-list/ability-list.component.ts
+++ src/app/components/ability-list/ability-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeService } from '../../services/poke.service';
+import { PokeHelperService } from '../../services/poke-helper.service';
+import { AbilitiesDetails } from '../../models/abilities-details';
 
 @Component({
   selector: 'app-ability-list',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './ability-list.component.html',
   styleUrl: './ability-list.component.css'
 })
-export class AbilityListComponent {
+export class AbilityListComponent implements OnInit {
+  ability: AbilitiesDetails[];
 
+  constructor(private pokeService: PokeService,  private pokeHelperService: PokeHelperService) {
+    this.ability = [];
+  }
+
+  ngOnInit(): void {
+    this.getAbilities();
+  }
+
+  getAbilities() {
+    this.pokeService.getAbilities().subscribe((data: any) => {
+      this.ability = data.results;
+      console.log(this.ability);
+    });
+  }
 }
