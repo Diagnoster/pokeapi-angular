6e3bd2f7689d5c0c41f1cf023c550b6591cc69d2
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-03-15 11:33:22 -0300

    add nature component

---
+++ src/app/components/nature-list/nature-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nature-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './nature-list.component.html',
+  styleUrl: './nature-list.component.css'
+})
+export class NatureListComponent {
+
+}
