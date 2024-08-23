63cb4d63bdad3b6e9f9eb5be183e96da1de8e02e
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2024-08-23 17:54:06 -0300

    feat: adjust for waiting for browser rendering.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
@@ -12,33 +13,35 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
-    // to perform opacity on divs
-    const containers = document.querySelectorAll('.pokedex-container, .move-container, .items-container');
+    // waiting for browser rendering
+    if (isPlatformBrowser(this.platformId)) {
+      const containers = document.querySelectorAll('.pokedex-container, .move-container, .items-container');
 
-    containers.forEach(container => {
-      const element = container as HTMLElement;
+      containers.forEach(container => {
+        const element = container as HTMLElement;
 
-      element.addEventListener('mouseover', () => {
-        containers.forEach(c => {
-          const otherElement = c as HTMLElement;
-          if (otherElement !== element) {
-            otherElement.style.opacity = '0.5';
-          }
+        element.addEventListener('mouseover', () => {
+          containers.forEach(c => {
+            const otherElement = c as HTMLElement;
+            if (otherElement !== element) {
+              otherElement.style.opacity = '0.5';
+            }
+          });
+        });
+
+        element.addEventListener('mouseout', () => {
+          containers.forEach(c => {
+            const otherElement = c as HTMLElement;
+            otherElement.style.opacity = '1';
+          });
         });
       });
-
-      element.addEventListener('mouseout', () => {
-        containers.forEach(c => {
-          const otherElement = c as HTMLElement;
-          otherElement.style.opacity = '1';
-        });
-      });
-    });
+    }
   }
-
 }
